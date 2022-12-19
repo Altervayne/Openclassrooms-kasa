c6f03bc1802ffca3a0d7b298536be9cffb3c2c64
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-20 00:24:50 +0100

    forgot to add 404 page files

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,18 @@
+import '../styles/PageNotFound.css';
+import Header from '../components/Header.jsx'
+
+function PageNotFound({activeLink}) {
+    return (
+        <div className="App">
+            <Header activeLink={activeLink} />
+                <main className='page-not-found'>
+                    <h1 className='page-not-found__number'>404</h1>
+                    <h2 className='page-not-found__oops'>Oups! La page que vous demandez n'existe pas.</h2>
+                    <a href='./' className='page-not-found__back'>Retourner sur la page d'accueil</a>
+                </main>
+        </div>
+  );
+}
+
+
+export default PageNotFound;
