6e2454c74feddf60083d403464a1b2d1f13ff3e6
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-20 00:55:09 +0100

    changed image display method to background-image style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,7 @@ import aboutBanner from '../assets/kasa-about-banner.jpg'
 
 function Banner({activeLink}) {
     return ( 
-            <div className='banner__container'>
-                    <img className='banner__image' src={activeLink === 'home' ? homeBanner : aboutBanner} alt='Photographie de paysage de vacances'></img>
+            <div className='banner__container' style={activeLink === 'home' ? {backgroundImage: `url(${homeBanner})`} : {backgroundImage: `url(${aboutBanner})`} }>
                     { activeLink === 'home' ? <h1 className='banner__title'>Chez vous, partout et ailleurs</h1> : null }
             </div>
      )
--- src/components/HousingCard.jsx
+++ src/components/HousingCard.jsx
@@ -2,8 +2,7 @@ import '../styles/HousingCard.css'
 
 function HousingCard({ housing }) {
     return (
-        <div className="housing-card__container">
-            <img src={housing.cover} alt={housing.title} className="housing-card__image"></img>
+        <div className="housing-card__container" style={{backgroundImage: `url(${housing.cover})`}}>
             <h2 className="housing-card__title">{housing.title}</h2>
         </div>
     )
