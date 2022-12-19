34615ec38be24fdba0b76fe787958090563a91c2
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-19 09:31:11 +0100

    Set up housings carousel with placeholders on homepage

---
+++ src/components/HousingCard.jsx
@@ -0,0 +1,12 @@
+import '../styles/HousingCard.css'
+
+function HousingCard({housing}) {
+    return (
+        <div className="housing-card__container">
+            <img src={housing.cover} alt={housing.title} className="housing-card__image"></img>
+            <h2 className="housing-card__title">{ housing.title }</h2>
+        </div>
+    )
+}
+
+export default HousingCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,18 @@ import '../styles/Home.css';
 import Header from '../components/Header.jsx'
 import Banner from '../components/Banner.jsx'
 import Footer from '../components/Footer.jsx'
+import HousingCard from '../components/HousingCard.jsx'
+
+const housingList = [
+  {
+    title: "Appartement cosy",
+		cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
+  },
+  {
+    title: "Magnifique appartement proche Canal Saint Martin",
+		cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg"
+  }
+]
 
 function Home() {
   const activeLink = 'home'
@@ -11,12 +23,19 @@ function Home() {
       <Header activeLink={activeLink} />
       <main>
         <Banner activeLink={activeLink} />
-        <div className="homeCarousel">
+
+        <div className="home__carousel">
+          <HousingCard housing={housingList[0]} />
+          <HousingCard housing={housingList[1]} />
+          <HousingCard housing={housingList[0]} />
+          <HousingCard housing={housingList[1]} />
         </div>
+
       </main>
       <Footer />
     </div>
   );
 }
+
 
 export default Home;
