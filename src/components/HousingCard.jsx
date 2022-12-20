bf1bae96b4391f621c9786189dbb4bd2dc4562f0
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-20 23:38:00 +0100

    set up homepage carousel links

--- src/components/HousingCard.jsx
+++ src/components/HousingCard.jsx
@@ -3,7 +3,9 @@ import '../styles/HousingCard.css'
 function HousingCard({ housing }) {
     return (
         <div className="housing-card__container" style={{backgroundImage: `url(${housing.cover})`}}>
-            <h2 className="housing-card__title">{housing.title}</h2>
+            <a href={`./${housing.id}`} className='housing-card__link'>
+                <h2 className="housing-card__title">{housing.title}</h2>
+            </a>
         </div>
     )
 }
