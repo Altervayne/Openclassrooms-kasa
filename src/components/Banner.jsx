2af420064a5da4ddf9be299a07bbb96388677d62
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-20 21:56:56 +0100

    set up banner component responsive style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import aboutBanner from '../assets/kasa-about-banner.jpg'
 function Banner({activeLink}) {
     return ( 
             <div className='banner__container' style={activeLink === 'home' ? {backgroundImage: `url(${homeBanner})`} : {backgroundImage: `url(${aboutBanner})`} }>
-                    { activeLink === 'home' ? <h1 className='banner__title'>Chez vous, partout et ailleurs</h1> : null }
+                    { activeLink === 'home' ? (<><h1 className='banner__title'>Chez vous, partout et ailleurs</h1><h1 className='banner__title__mobile'>Chez vous,<br/> partout et ailleurs</h1></>) : null }
             </div>
      )
 }
