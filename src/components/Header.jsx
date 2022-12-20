d45e33dd92544e906a117ab9d16bb5354ebd821e
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-20 01:26:19 +0100

    homepage responsive completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ import logoKasa from '../assets/kasa-logo.svg'
 function Header({activeLink}) {
     return (
         <header>
-            <img src={ logoKasa } alt='Logo de Kasa'></img>
+            <img src={ logoKasa } alt='Logo de Kasa' className='header__logo'></img>
             <nav>
                 <a href="/" className={activeLink === 'home' ? "nav__link__active" : "nav__link"}>Accueil</a>
                 <a href="/about" className={activeLink === 'about' ? "nav__link__active" : "nav__link"}>A Propos</a>
+                <a href="/" className={activeLink === 'home' ? "nav__link__caps-active" : "nav__link__caps"}>ACCUEIL</a>
+                <a href="/about" className={activeLink === 'about' ? "nav__link__caps-active" : "nav__link__caps"}>A PROPOS</a>
             </nav>
         </header>
     )
