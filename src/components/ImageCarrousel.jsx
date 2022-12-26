bd4903dd65041c1a880254249fdeffc79b222a72
Altervayne <79285331+Altervayne@users.noreply.github.com>
2022-12-26 15:04:39 +0100

    added check on picture list to not show number and arrows when only one picture is present

--- src/components/ImageCarrousel.jsx
+++ src/components/ImageCarrousel.jsx
@@ -4,6 +4,11 @@ import arrowLeft from '../assets/arrow-left.svg'
 import arrowRight from '../assets/arrow-right.svg'
 
 function ImageCarrousel({pictureList}) {
+    let singlePicture = false
+    if (pictureList.length === 1) {
+        singlePicture = true
+    }
+
     const [pictureIndex, changeIndex] = useState(1)
     const updateIndex = (direction) => {
         direction === 'left' ? (
@@ -15,11 +20,15 @@ function ImageCarrousel({pictureList}) {
 
     return (
         <div className='image-carrousel__container' style={{backgroundImage: `url(${pictureList[pictureIndex-1]})`}}>
-            <img src={arrowLeft} alt='' className='image-carrousel__arrow' onClick={() => updateIndex('left')}></img>
-            <div className='image-number'>
-                <p classname='image-number__index'>{ pictureIndex }/{ pictureList.length }</p>
-            </div>
-            <img src={arrowRight} alt='' className='image-carrousel__arrow' onClick={() => updateIndex('right')}></img>
+            { singlePicture ? null : (
+                <>
+                    <img src={arrowLeft} alt='' className='image-carrousel__arrow' onClick={() => updateIndex('left')}></img>
+                    <div className='image-number'>
+                        <p classname='image-number__index'>{ pictureIndex }/{ pictureList.length }</p>
+                    </div>
+                    <img src={arrowRight} alt='' className='image-carrousel__arrow' onClick={() => updateIndex('right')}></img>
+                </>
+            )}
         </div>
     )
 }
